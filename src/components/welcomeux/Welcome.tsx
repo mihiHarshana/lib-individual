b27e261be6f2847a5f699650d8da7cc6efb75bcc
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-02-23 20:58:35 +0530

    add railway font add media query to welcome UX

--- src/components/welcomeux/Welcome.tsx
+++ src/components/welcomeux/Welcome.tsx
@@ -5,13 +5,13 @@ import WelcomeImage from "../../assests/images/welcomeImage.jpg"
 const Welcome = () => {
   return (
     <Row>
-      <Col xs={12} className="p-0 py-2">
+      <Col xs={12} className="p-0 sm-py-2">
         <h1 className="text-center">My Library</h1>
       </Col>
       <Col xs={12} className="p-0">
         <Image className="img" src={WelcomeImage} />
       </Col>
-      <Col xs={12} className="text-end pe-5">
+      <Col xs={12} className="text-end pe-5 test1">
         Photo Credits <a href="https://unsplash.com/photos/ajE5goOGzZc" >Anna Hunko</a>
       </Col>
     </Row>
