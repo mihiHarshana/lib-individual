e10ab4676821a7ac3ff0a180c1c19c530a5194b7
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-02-23 20:33:49 +0530

    Design Welcome UX

---
+++ src/MainApp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "./components/welcomeux/Welcome";
+import {Container} from "react-bootstrap";
+
+
+const MainApp = () => {
+  return (
+    <Container fluid>
+      <Welcome />
+    </Container>
+
+  )
+}
+export default MainApp
---
+++ src/components/welcomeux/Welcome.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Col, Image, Row} from "react-bootstrap";
+import WelcomeImage from "../../assests/images/welcomeImage.jpg"
+
+const Welcome = () => {
+  return (
+    <Row>
+      <Col xs={12} className="p-0 py-2">
+        <h1 className="text-center">My Library</h1>
+      </Col>
+      <Col xs={12} className="p-0">
+        <Image className="img" src={WelcomeImage} />
+      </Col>
+      <Col xs={12} className="text-end pe-5">
+        Photo Credits <a href="https://unsplash.com/photos/ajE5goOGzZc" >Anna Hunko</a>
+      </Col>
+    </Row>
+
+  )
+
+}
+export default Welcome
