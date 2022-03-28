65d3f874bbfa1ff70039b662d802261560e46664
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-03-28 08:43:41 +0530

    title chaned to Books, font changed

--- src/components/books/BookTitle.tsx
+++ src/components/books/BookTitle.tsx
@@ -4,7 +4,7 @@ import {Col} from "react-bootstrap";
 const BookTitle = () => {
   return (
     <Col xs={12} className="my-0">
-      <h2 className="p-0 m-0">Book</h2>
+      <h2 className="p-0 m-0 title">Books</h2>
       <span className="me-xl-5 me-lg-4 me-3">
         <hr className="mt-1 mb-0 mb-0 line"/>
       </span>
